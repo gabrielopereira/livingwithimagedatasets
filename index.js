ece058bb54ccd5e6e173e0487dea9b3d733ef92f
Gabriel Pereira <gabrielopereira@gmail.com>
2022-09-12 11:31:57 +0100

    changed datasets, new code to handle

--- index.js
+++ index.js
@@ -13,10 +13,91 @@ function createPDF(numPages){
 
   for (let i = 0; i < numPages; i++) {
 
-    pdfDoc.image('tinyIMG/tiny_' + Math.floor(Math.random() * 100000) + '.JPEG', {width: 300, height: 300});
+    var imgNumber = Math.floor(Math.random() * 8) + 1;
 
-    pdfDoc.moveDown(12)
-    pdfDoc.text('Images to live with.', {align: 'right'})
+    if (imgNumber == 1){
+
+      var img = Math.floor(Math.random() * 100) + 1;
+      pdfDoc.image('datasets/1 - COIL-100/img' + img + '.png', {fit: [300, 300]});
+
+      pdfDoc.moveDown(10)
+      pdfDoc.text('Images to live with.', {align: 'right'})
+      pdfDoc.moveDown(0.5)
+
+      pdfDoc.text('From the Columbia University Image Library COIL-100 dataset.', {align: 'right'})
+    } else if (imgNumber == 2){
+
+      var img = Math.floor(Math.random() * 11380) + 1;
+      pdfDoc.image('datasets/2 - ImageNet/img' + img + '.jpg', {fit: [300, 300]});
+
+      pdfDoc.moveDown(10)
+      pdfDoc.text('Images to live with.', {align: 'right'})
+      pdfDoc.moveDown(0.5)
+
+      pdfDoc.text('From the TinyImageNet dataset.', {align: 'right'})
+    } else if (imgNumber == 3){
+
+      var img = Math.floor(Math.random() * 75) + 1;
+      pdfDoc.image('datasets/3 - Labelled Faces in the Wild (LFW) Dataset/img' + img + '.jpg', {fit: [300, 300]});
+
+      pdfDoc.moveDown(10)
+      pdfDoc.text('Images to live with.', {align: 'right'})
+      pdfDoc.moveDown(0.5)
+
+      pdfDoc.text('From the Labelled Faces in the Wild (LFW) dataset.', {align: 'right'})
+    } else if (imgNumber == 4){
+
+      var img = Math.floor(Math.random() * 37) + 1;
+      pdfDoc.image('datasets/4 - Leaf Dataset/img' + img + '.JPG', {fit: [300, 300]});
+
+      pdfDoc.moveDown(10)
+      pdfDoc.text('Images to live with.', {align: 'right'})
+      pdfDoc.moveDown(0.5)
+
+
+      pdfDoc.text('From the Leaf dataset.', {align: 'right'})
+    } else if (imgNumber == 5){
+
+      var img = Math.floor(Math.random() * 180) + 1;
+      pdfDoc.image('datasets/5 - Open Image/img' + img + '.jpg', {fit: [300, 300]});
+
+      pdfDoc.moveDown(10)
+      pdfDoc.text('Images to live with.', {align: 'right'})
+      pdfDoc.moveDown(0.5)
+
+      pdfDoc.text('From the Open Image dataset.', {align: 'right'})
+    } else if (imgNumber == 6){
+
+      var img = Math.floor(Math.random() * 190) + 1;
+      pdfDoc.image('datasets/6 - Stanford Dogs Datasets/img' + img + '.jpg', {fit: [300, 300]});
+
+      pdfDoc.moveDown(10)
+      pdfDoc.text('Images to live with.', {align: 'right'})
+      pdfDoc.moveDown(0.5)
+
+      pdfDoc.text('From the Stanford Dogs dataset.', {align: 'right'})
+    } else if (imgNumber == 7){
+
+      var img = Math.floor(Math.random() * 365) + 1;
+      pdfDoc.image('datasets/7 - Tencent/img' + img + '.jpg', {fit: [300, 300]});
+
+      pdfDoc.moveDown(10)
+      pdfDoc.text('Images to live with.', {align: 'right'})
+      pdfDoc.moveDown(0.5)
+
+      pdfDoc.text('From the Tencent dataset.', {align: 'right'})
+    } else if (imgNumber == 8){
+
+      var img = Math.floor(Math.random() * 2100) + 1;
+      pdfDoc.image('datasets/8 - VisualGenome/img' + img + '.jpg', {fit: [300, 300]});
+
+      pdfDoc.moveDown(10)
+      pdfDoc.text('Images to live with.', {align: 'right'})
+      pdfDoc.moveDown(0.5)
+
+      pdfDoc.text('From the VisualGenome dataset.', {align: 'right'})
+    };
+
     pdfDoc.moveDown(0.5)
     pdfDoc.text('Read the article on Photographies Journal.', {
      link: 'http://ADDLINK',
