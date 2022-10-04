ecd40794bf9a39ed1345be0c41e893ad5e5400b1
Gabriel Pereira <gabrielopereira@gmail.com>
2022-10-04 22:41:56 +0100

    fix async error

--- app.js
+++ app.js
@@ -18,29 +18,34 @@ app.get('/', (req, res) => {
 // POST request for creating Genre.
 app.post('/generate', function(req, res, next) {
 
-  console.log(req.body.generateNumber);
+  async function generatePDF() {
 
-  const result = getPDF.createPDF(req.body.generateNumber);
+    // generate our PDFDocument
+    console.log(req.body.generateNumber);
+    let result = await getPDF.createPDF(req.body.generateNumber);
 
-  var filename = "SampleDocument.pdf";
-  var stream = fs.createReadStream(filename);
-  // Be careful of special characters
+    var filename = "SampleDocument.pdf";
+    var stream = fs.createReadStream(filename);
+    // Be careful of special characters
+    filename = encodeURIComponent(filename);
+    // Ideally this should strip them
 
-  filename = encodeURIComponent(filename);
-  // Ideally this should strip them
+    res.setHeader('Content-disposition', 'inline; filename="' + filename + '"');
+    res.setHeader('Content-type', 'application/pdf');
 
-  res.setHeader('Content-disposition', 'inline; filename="' + filename + '"');
-  res.setHeader('Content-type', 'application/pdf');
+    stream.on('open', function () {
+       // This just pipes the read stream to the response object (which goes to the client)
+       stream.pipe(res);
+     });
 
-  stream.on('open', function () {
-     // This just pipes the read stream to the response object (which goes to the client)
-     stream.pipe(res);
-   });
+     // This catches any errors that happen while creating the readable stream (usually invalid names)
+     stream.on('error', function(err) {
+       res.end(err);
+     });
 
-   // This catches any errors that happen while creating the readable stream (usually invalid names)
-   stream.on('error', function(err) {
-     res.end(err);
-   });
+  }
+
+  generatePDF();
 
 });
 
